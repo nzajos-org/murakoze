3c32f50982b80bacb075aa79d5072935f7df0c82
Josue Nzajyayo <nzajos@Josues-MacBook-Pro.local>
2022-04-10 03:22:14 +0900

    test webhk

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func deleteMember(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//test webhookkkkk
+//test webhookkkkkkk
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
