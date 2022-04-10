4a7ab7e714d7933f09c0e65353b17b3d22110586
Josue Nzajyayo <nzajos@Josues-MacBook-Pro.local>
2022-04-10 18:37:34 +0900

    test webhkl89polikjlkjlkljkkjkjhkjj

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func deleteMember(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//test webhookkkkkklllhtkljkjlkjkjhnkjhjgkjh
+//test webhookkkkkklllhtkljkjlkjkjhnkjhjgkjhhg
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
